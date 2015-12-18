91343107316971f5f58791770db348b6c2689a76
Martino Facchin <m.facchin@arduino.cc>
2015-12-18 12:48:56 +0100

    add submodule and fix comple scripts

--- compile_linux.sh
+++ compile_linux.sh
@@ -1,1 +1,1 @@
-cd libserialport && ./configure && make clean && make && cd .. && gcc main.c -Ilibserialport/ -Llibserialport/.libs/ -lserialport -static -o listSerialC && cp listSerialC distrib/linux/listSerialC_64
+mkdir -p distrib/linux && cd libserialport && ./configure && make clean && make && cd .. && gcc main.c -Ilibserialport/ -Llibserialport/.libs/ -lserialport -static -o listSerialC && cp listSerialC distrib/linux/listSerialC_64
