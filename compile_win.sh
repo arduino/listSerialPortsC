06af65be45a3b50d44fb645283cef3e249870ef7
Martino Facchin <m.facchin@arduino.cc>
2015-12-18 12:46:42 +0100

    initial commit

---
+++ compile_win.sh
@@ -0,0 +1,1 @@
+cd libserialport && ./configure --host=i686-w64-mingw32 && make clean && make &&cd .. && i686-w64-mingw32-gcc main.c -Ilibserialport/ -Llibserialport/.libs/ -lserialport -lsetupapi -static -o listSerialC.exe && cp listSerialC.exe distrib/windows/
---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <libserialport.h>
+
+int main(void)
+{
+    int i;
+    struct sp_port **ports;
+
+    sp_list_ports(&ports);
+
+    for (i = 0; ports[i]; i++) {
+        int vid, pid;
+        if (sp_get_port_usb_vid_pid(ports[i], &vid, &pid) == SP_OK) {
+            printf("%s | %04X:%04X | %s\n", sp_get_port_name(ports[i]), vid, pid, sp_get_port_usb_serial(ports[i]));
+        }
+    }
+
+    sp_free_port_list(ports);
+}
+
