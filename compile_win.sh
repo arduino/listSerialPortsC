80629d9d09d1841c721bd1d684c53e8c8721cb6e
Martino Facchin <m.facchin@arduino.cc>
2015-12-21 18:27:45 +0100

    fix Windows build
    jni build need the followinfg file under /opt/jvm/jdk1.8.0/include/win32/
    
    /*
     * @(#)jni_md.h	1.13 03/01/23
     *
     * Copyright 2003 Sun Microsystems, Inc. All rights reserved.
     * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
     */
    
    \#ifdef MSW
    \#include <windows.h>
    \#endif
    
    \#ifndef _JAVASOFT_JNI_MD_H_
    \#define _JAVASOFT_JNI_MD_H_
    
    \#define JNIEXPORT __declspec(dllexport)
    \#define JNIIMPORT __declspec(dllimport)
    \#define JNICALL __stdcall
    
    typedef long jint;
    typedef __int64 jlong;
    typedef signed char jbyte;
    
    \#endif /* !_JAVASOFT_JNI_MD_H_ */

--- compile_win.sh
+++ compile_win.sh
@@ -1,3 +1,3 @@
-mkdir -p distrib/windows && cd libserialport && ./configure --host=i686-w64-mingw32 && make clean && make && cd .. && i686-w64-mingw32-gcc main.c libserialport/windows.c libserialport/serialport.c -Ilibserialport/ -lsetupapi -static -o listSerialC.exe && cp listSerialC.exe distrib/windows/
-i686-w64-mingw32-gcc  jnilib.c libserialport/windows.c libserialport/serialport.c -lsetupapi -Ilibserialport/ -I/opt/jvm/jdk1.8.0/include/ -I/opt/jvm/jdk1.8.0/include/linux/ -shared -o listSerialsj.dll && cp listSerialsj.dll distrib/windows/
+mkdir -p distrib/windows && cd libserialport && ./configure --host=i686-w64-mingw32 && make clean && make && cd .. && i686-w64-mingw32-gcc main.c -Llibserialport/.libs/ -Ilibserialport/ -lserialport -lsetupapi -static -o listSerialC.exe && cp listSerialC.exe distrib/windows/
+i686-w64-mingw32-gcc -D_JNI_IMPLEMENTATION_ -Wl,--kill-at jnilib.c libserialport/.libs/libserialport.a -lsetupapi -Ilibserialport/ -I/opt/jvm/jdk1.8.0/include/ -I/opt/jvm/jdk1.8.0/include/win32/ -shared -o listSerialsj.dll && cp listSerialsj.dll distrib/windows/
 
