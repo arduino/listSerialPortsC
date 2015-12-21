68ecc090e190a2f97ee64afeb85c29aee544617f
Martino Facchin <m.facchin@arduino.cc>
2015-12-21 21:00:44 +0000

    update pack_and_release script

--- pack_and_release.sh
+++ pack_and_release.sh
@@ -1,9 +1,19 @@
 VERSION=$1
+
+if [ x$VERSION = "x" ]
+then
+echo "Please provide a version number"
+exit 1
+fi
+
 ./compile_win.sh
 ./compile_linux.sh
 ./compile_mac.sh
 
-mv distrib liblistSerials
-zip -r liblistSerials-$VERSION.zip  liblistSerials/
+mv distrib liblistSerials-$VERSION
+zip -r liblistSerials-$VERSION.zip  liblistSerials-$VERSION
 
-rm -rf liblistSerials
+echo scp liblistSerials-$VERSION.zip root@downloads-02.arduino.cc:/var/www/files/liblistSerials/
+shasum liblistSerials-$VERSION.zip
+
+rm -rf liblistSerials-$VERSION
