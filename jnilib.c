c97fa112c62d08b9b7685d9b00eb45e2d14c5790
Martino Facchin <m.facchin@arduino.cc>
2016-01-11 13:08:24 +0100

    simplify jni resolve function

--- jnilib.c
+++ jnilib.c
@@ -6,30 +6,21 @@
 JNIEXPORT jstring JNICALL Java_processing_app_Platform_resolveDeviceAttachedToNative
   (JNIEnv * env, jobject jobj, jstring serial)
 {
-	int i;
-    struct sp_port **ports;
+	struct sp_port *port;
 
-    char vid_pid_iserial[256] = " ";
-	const char *port = (*env)->GetStringUTFChars(env, serial, NULL);
+	char vid_pid_iserial[256] = " ";
+	const char *portname = (*env)->GetStringUTFChars(env, serial, NULL);
 	jstring result;
 
-    sp_list_ports(&ports);
+	sp_get_port_by_name(portname, &port);
 
-    for (i = 0; ports[i]; i++) {
-    	if (strncmp(port, sp_get_port_name(ports[i]), strlen(port)) == 0) {
-	        int vid, pid;
-			//printf("%s %d\n", ports[i], sp_get_port_usb_vid_pid(ports[i], &vid, &pid));
-	        if (sp_get_port_usb_vid_pid(ports[i], &vid, &pid) == SP_OK) {
-	        	snprintf(vid_pid_iserial, sizeof(vid_pid_iserial), "0x%04X_0x%04X_%s", vid, pid, sp_get_port_usb_serial(ports[i]));
-	            break;
-	        }
-	    } else {
-	    	continue;
-	    }
-    }
+	int vid, pid;
+	if (sp_get_port_usb_vid_pid(port, &vid, &pid) == SP_OK) {
+		snprintf(vid_pid_iserial, sizeof(vid_pid_iserial), "0x%04X_0x%04X_%s", vid, pid, sp_get_port_usb_serial(port));
+	}
 
-    sp_free_port_list(ports);
-    (*env)->ReleaseStringUTFChars(env, serial, port);
+	sp_free_port(port);
+	(*env)->ReleaseStringUTFChars(env, serial, portname);
 
-    return (*env)->NewStringUTF(env, vid_pid_iserial);
+	return (*env)->NewStringUTF(env, vid_pid_iserial);
 }
