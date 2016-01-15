6380c63dc1438f51b8cdf334979d73f7aecd01a0
Martino Facchin <m.facchin@arduino.cc>
2016-01-15 15:57:03 +0100

    add advanced listPort jni call

--- jnilib.c
+++ jnilib.c
@@ -24,3 +24,35 @@ JNIEXPORT jstring JNICALL Java_processing_app_Platform_resolveDeviceAttachedToNa
 
 	return (*env)->NewStringUTF(env, vid_pid_iserial);
 }
+
+JNIEXPORT jobjectArray JNICALL Java_processing_app_Platform_listSerialsNative
+  (JNIEnv * env, jobject jobj)
+{
+	struct sp_port **ports;
+	jobjectArray ret;
+	int i;
+
+	char portname_vid_pid[256] = " ";
+
+	sp_list_ports(&ports);
+
+	// like ports.size()
+	for (i = 0; ports[i]; i++) {};
+
+	ret = (jobjectArray)(*env)->NewObjectArray(env, i, (*env)->FindClass(env, "java/lang/String"), (*env)->NewStringUTF(env, ""));
+
+	int vid, pid;
+	for (i = 0; ports[i]; i++) {
+		int vid, pid;
+		if (sp_get_port_usb_vid_pid(ports[i], &vid, &pid) == SP_OK) {
+			snprintf(portname_vid_pid, sizeof(portname_vid_pid), "%s_%04X_%04X", sp_get_port_name(ports[i]), vid, pid);
+		} else {
+			snprintf(portname_vid_pid, sizeof(portname_vid_pid), "%s_%04X_%04X", sp_get_port_name(ports[i]), 0, 0);
+		}
+		(*env)->SetObjectArrayElement(env, ret, i, (*env)->NewStringUTF(env, portname_vid_pid));
+	}
+
+	sp_free_port_list(ports);
+
+	return ret;
+}
