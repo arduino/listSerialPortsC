e999f014143fac629afefeb7e7d4e903c5005769
Nico <NicoHood@users.noreply.github.com>
2016-01-20 20:27:14 +0100

    Better build script
    Added -x bash option
    added autoconf
    removed &&

--- compile_linux.sh
+++ compile_linux.sh
@@ -1,6 +1,23 @@
-mkdir -p distrib/linux64 && cd libserialport && ./configure && make clean && make && cd .. && gcc main.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/  -o listSerialC && cp listSerialC distrib/linux64/listSerialC
+#!/bin/bash -x
+
+mkdir -p distrib/linux64
+cd libserialport
+autoconf
+./configure
+make clean
+make
+cd ..
+gcc main.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/  -o listSerialC
+cp listSerialC distrib/linux64/listSerialC
 gcc jnilib.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/ -I/opt/jvm/jdk1.8.0/include/ -I/opt/jvm/jdk1.8.0/include/linux/ -shared -fPIC -o liblistSerialsj.so && cp liblistSerialsj.so distrib/linux64/
 
-mkdir -p distrib/linux32 && cd libserialport && CFLAGS=-m32 ./configure && make clean && make && cd .. && gcc -m32 main.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/  -o listSerialC && cp listSerialC distrib/linux32/listSerialC
+mkdir -p distrib/linux32
+cd libserialport
+CFLAGS=-m32 ./configure
+make clean
+make
+cd ..
+gcc -m32 main.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/  -o listSerialC
+cp listSerialC distrib/linux32/listSerialC
 gcc -m32 jnilib.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/ -I/opt/jvm/jdk1.8.0/include/ -I/opt/jvm/jdk1.8.0/include/linux/ -shared -fPIC -o liblistSerialsj.so && cp liblistSerialsj.so distrib/linux32
 
