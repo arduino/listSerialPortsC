5e453fa5f83454b30789324c57c599b654de2603
NicoHood <NicoHood@users.noreply.github.com>
2016-01-20 20:47:04 +0100

    Build script fixes

--- compile_linux.sh
+++ compile_linux.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -xe
 
 mkdir -p distrib/linux64
 cd libserialport
@@ -9,7 +9,8 @@ make
 cd ..
 gcc main.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/  -o listSerialC
 cp listSerialC distrib/linux64/listSerialC
-gcc jnilib.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/ -I/opt/jvm/jdk1.8.0/include/ -I/opt/jvm/jdk1.8.0/include/linux/ -shared -fPIC -o liblistSerialsj.so && cp liblistSerialsj.so distrib/linux64/
+gcc jnilib.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/ -I/usr/lib/jvm/java-1.8.0-openjdk-amd64/include/ -I/usr/lib/jvm/java-1.8.0-openjdk-amd64/include/linux/ -shared -fPIC -o liblistSerialsj.so
+cp liblistSerialsj.so distrib/linux64/
 
 mkdir -p distrib/linux32
 cd libserialport
@@ -17,7 +18,8 @@ CFLAGS=-m32 ./configure
 make clean
 make
 cd ..
-gcc -m32 main.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/  -o listSerialC
+gcc -m32 main.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/ -o listSerialC
 cp listSerialC distrib/linux32/listSerialC
-gcc -m32 jnilib.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/ -I/opt/jvm/jdk1.8.0/include/ -I/opt/jvm/jdk1.8.0/include/linux/ -shared -fPIC -o liblistSerialsj.so && cp liblistSerialsj.so distrib/linux32
+gcc -m32 jnilib.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/ -I/usr/lib/jvm/java-1.8.0-openjdk-amd64/include/ -I/usr/lib/jvm/java-1.8.0-openjdk-amd64/include/linux/ -shared -fPIC -o liblistSerialsj.so
+cp liblistSerialsj.so distrib/linux32
 
