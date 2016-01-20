4b238b580b1f002baf3be4e7c20c2e5a5c4e75e9
Nico <NicoHood@users.noreply.github.com>
2016-01-20 20:27:14 +0100

    modernize build scripts
    Add ARM build

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-Simple environment to test libserialport in a single build machine fashion
+_Simple environment to test libserialport in a single build machine fashion_
 
 Prerequisites:
-* An osxcross installation
+* An [osxcross](https://github.com/tpoechtrager/osxcross) installation
 * An updated mingw32 environment
 
+TLTR:
+```bash
+dpkg --add-architecture i386
+apt-get install build-essential gcc-multilib g++-multilib mingw-w64 xz-utils libxml2-dev clang patch git gcc-4.8-arm-linux-gnueabihf g++-4.8-arm-linux-gnueabihf autoconf libtool linux-libc-dev:i386 gcc-arm-linux-gnueabihf zip
+#setup osxcross + add binaries to PATH
+git clone https://github.com/facchinm/listSerialPortsC --recursive
+./pack_and_release.sh
+```
+
 TODO:
-* Add a jni wrapper
 * Add a golang wrapper
--- compile_linux.sh
+++ compile_linux.sh
@@ -1,6 +1,39 @@
-mkdir -p distrib/linux64 && cd libserialport && ./configure && make clean && make && cd .. && gcc main.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/  -o listSerialC && cp listSerialC distrib/linux64/listSerialC
-gcc jnilib.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/ -I/opt/jvm/jdk1.8.0/include/ -I/opt/jvm/jdk1.8.0/include/linux/ -shared -fPIC -o liblistSerialsj.so && cp liblistSerialsj.so distrib/linux64/
+#!/bin/bash -xe
 
-mkdir -p distrib/linux32 && cd libserialport && CFLAGS=-m32 ./configure && make clean && make && cd .. && gcc -m32 main.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/  -o listSerialC && cp listSerialC distrib/linux32/listSerialC
-gcc -m32 jnilib.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/ -I/opt/jvm/jdk1.8.0/include/ -I/opt/jvm/jdk1.8.0/include/linux/ -shared -fPIC -o liblistSerialsj.so && cp liblistSerialsj.so distrib/linux32
+JAVA_INCLUDE_PATH=/opt/jvm/jdk1.8.0/include/
 
+mkdir -p distrib/linux64
+cd libserialport
+./autogen.sh
+./configure
+make clean
+make
+cd ..
+gcc main.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/  -o listSerialC
+cp listSerialC distrib/linux64/listSerialC
+gcc jnilib.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/ -I$JAVA_INCLUDE_PATH -I$JAVA_INCLUDE_PATH/linux/ -shared -fPIC -o liblistSerialsj.so
+cp liblistSerialsj.so distrib/linux64/
+
+mkdir -p distrib/linux32
+cd libserialport
+./autogen.sh
+CFLAGS=-m32 ./configure
+make clean
+make
+cd ..
+gcc -m32 main.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/ -o listSerialC
+cp listSerialC distrib/linux32/listSerialC
+gcc -m32 jnilib.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/ -I$JAVA_INCLUDE_PATH -I$JAVA_INCLUDE_PATH/linux/ -shared -fPIC -o liblistSerialsj.so
+cp liblistSerialsj.so distrib/linux32
+
+mkdir -p distrib/arm
+cd libserialport
+./autogen.sh
+./configure --host=arm-linux-gnueabihf
+make clean
+make
+cd ..
+arm-linux-gnueabihf-gcc main.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/  -o listSerialC
+cp listSerialC distrib/arm/listSerialC
+arm-linux-gnueabihf-gcc jnilib.c libserialport/linux_termios.c libserialport/linux.c libserialport/serialport.c -Ilibserialport/ -I$JAVA_INCLUDE_PATH -I$JAVA_INCLUDE_PATH/linux/ -shared -fPIC -o liblistSerialsj.so
+cp liblistSerialsj.so distrib/arm/
