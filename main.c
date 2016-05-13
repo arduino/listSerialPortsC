8ec99ca9241cad8e38b738c5174c9f66676062e5
Martino Facchin <m.facchin@arduino.cc>
2016-05-13 16:01:13 +0200

    fix main example using new API

--- main.c
+++ main.c
@@ -12,13 +12,17 @@ int main(void)
         return 0;
     }
 
-    for (i = 0; ports[i]; i++) {
-        int vid, pid;
-        if (sp_get_port_usb_vid_pid(ports[i], &vid, &pid) == SP_OK) {
-            printf("%s | %04X:%04X | %s %s %s\n", sp_get_port_name(ports[i]), vid, pid, sp_get_port_usb_serial(ports[i]), sp_get_port_usb_product(ports[i]), sp_get_port_usb_manufacturer(ports[i]));
-        }
-    }
+    struct sp_port *port;
+    char vid_pid_iserial[256] = " ";
 
+    for (i = 0; ports[i]; i++) {
+        sp_get_port_by_name(sp_get_port_name(ports[i]), &port);
+
+        int vid, pid;
+        if (sp_get_port_usb_vid_pid(port, &vid, &pid) == SP_OK) {
+            printf("%s | %04X:%04X | %s %s %s\n", sp_get_port_name(ports[i]), vid, pid, sp_get_port_usb_serial(port), sp_get_port_usb_product(port), sp_get_port_usb_manufacturer(port));
+        }
+        sp_free_port(port);
+    }
     sp_free_port_list(ports);
 }
-
