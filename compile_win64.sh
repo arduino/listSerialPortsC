6342b4a14ad71269ffb8dad52ef50593316ac905
Carsten Presser <carsten.presser@aquila-biolabs.de>
2018-04-23 14:09:27 +0200

    Add Windows x86-64 build

--- compile_win64.sh
+++ compile_win64.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -xe
 
 JAVA_INCLUDE_PATH=/usr/lib/jvm/java-8-openjdk-amd64/include/
-ARCH=i686-w64-mingw32
+ARCH=x86_64-w64-mingw32
 CC=$ARCH-gcc
 
-mkdir -p distrib/$ARCH
+mkdir -p distrib/$ARCH/
 cd libserialport
 ./autogen.sh
 ./configure --host=$ARCH
