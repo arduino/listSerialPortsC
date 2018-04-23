7f0afe693f916c2cd52949cc29c2dd58ae7cdbd2
Carsten Presser <carsten.presser@aquila-biolabs.de>
2018-04-23 14:08:18 +0200

    Strip .dll files to reduce file size

--- compile_win64.sh
+++ compile_win64.sh
@@ -3,6 +3,7 @@
 JAVA_INCLUDE_PATH=/usr/lib/jvm/java-8-openjdk-amd64/include/
 ARCH=x86_64-w64-mingw32
 CC=$ARCH-gcc
+STRIP=$ARCH-strip
 
 mkdir -p distrib/$ARCH/
 cd libserialport
@@ -14,4 +15,5 @@ cd ..
 $CC main.c -Llibserialport/.libs/ -Ilibserialport/ -lserialport -lsetupapi -static -o listSerialC.exe
 cp listSerialC.exe distrib/$ARCH/
 $CC -D_JNI_IMPLEMENTATION_ -Wl,--kill-at jnilib.c libserialport/.libs/libserialport.a -lsetupapi -Ilibserialport/ -I$JAVA_INCLUDE_PATH -Iwin32_jni -shared -o listSerialsj.dll
+$STRIP listSerialsj.dll
 cp listSerialsj.dll distrib/$ARCH/
